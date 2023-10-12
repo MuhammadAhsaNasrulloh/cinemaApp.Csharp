9f3518658b475f79f220742fdc3be536ba2ae339
Muhammad Ahsan Nasrulloh <106051351+MuhammadAhsaNasrulloh@users.noreply.github.com>
2023-10-12 20:16:35 +0700

    Form3.cs

--- cinemaApp/Form3.cs
+++ cinemaApp/Form3.cs
@@ -21,29 +21,68 @@ namespace cinemaApp
 
         private void btnOrder_Click(object sender, EventArgs e)
         {
-            
-            if(int.TryParse(txtUmur.Text, out int age))
+            int kembalian;
+string tiket = comboBox1.Text;
+string film = comboBox2.Text;
+
+if (int.TryParse(txtUmur.Text, out int age))
+{
+    if (age <= 16)
+    {
+        MessageBox.Show("Maaf umur anda kurang cukup");
+    }
+
+    if (string.IsNullOrEmpty(tiket))
+    {
+        MessageBox.Show("Tiket belum dipilih.");
+    }
+    if(string.IsNullOrEmpty(film))
+    {
+        MessageBox.Show("Maaf film yang anda pilih tidak ada");
+    }
+    else
+    {
+        if (int.TryParse(txtBayar.Text, out int bayar))
+        {
+            if (tiket == "VVIP")
             {
-                if(int.TryParse(txtBayar.Text, out int bayar))
+                if (bayar >= VVIP)
                 {
-                   if(bayar < VVIP)
-                    {
-                        MessageBox.Show("Uang anda Kurang cukup");
-                    } 
-                   if(int.TryParse(txtBayar.Text, out int kembalian))
-                    {
-                        if(bayar < VVIP)
-                        {
-                            kembalian = bayar - VVIP;
-                            Console.Write(kembalian);
-                        }
-                    }
+                    kembalian = bayar - VVIP;
+                    MessageBox.Show("Nama: " + txtNamaTiket.Text + "\nUmur :" + txtUmur.Text + "\nKembalian: " + kembalian.ToString() + "\nBayar :" + txtBayar.Text + "\nTiket :" + comboBox1.Text);
                 }
-                if (age < 17)
+                else if (bayar < VVIP)
                 {
-                    MessageBox.Show("Maaf umur anda belum cukup");
+                    MessageBox.Show("Maaf uang anda kurang");
                 }
             }
+            else if(tiket == "VIP")
+            {
+                if (bayar >= VIP)
+                {
+                    kembalian = bayar - VIP;
+                    MessageBox.Show("Nama: " + txtNamaTiket.Text + "\nUmur :" + txtUmur.Text + "\nKembalian: " + kembalian.ToString() + "\nBayar :"+ txtBayar.Text +"\nTiket :" + comboBox1.Text);
+                } else if (bayar < VIP)
+                {
+                    MessageBox.Show("Maaf uang anda kurang");
+                }
+            }
+            else if (tiket == "Reguler")
+            {
+                if (bayar >= Reguler)
+                {
+                    kembalian = bayar - Reguler;
+                    MessageBox.Show("Nama: " + txtNamaTiket.Text + "\nUmur :" + txtUmur.Text + "\nKembalian: " + kembalian.ToString() + "\nBayar :" + txtBayar.Text + "\nTiket :" + comboBox1.Text);
+                }
+                else if (bayar < Reguler)
+                {
+                    MessageBox.Show("Maaf uang anda kurang");
+                }
+            }
+        }
+    }
+           
+    
         }
     }
 }
